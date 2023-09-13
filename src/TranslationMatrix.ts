3087e6454bab2c8c6735cba3e0eb318976e42cd2
= <srburnes02@gmail.com>
2023-09-13 08:24:09 -0400

    Add materials

--- src/TranslationMatrix.ts
+++ src/TranslationMatrix.ts
@@ -1,4 +1,11 @@
 export const TranslationMatrix = {
-	"view": "看法",
-	"delete": "删除"
+	"view": "查看",
+	"delete": "删除",
+	"fly": "航行",
+	"cargo": "货物",
+	"fuel": "燃料",
+}
+
+export const MaterialTranslationMatrix = {
+	"RAT": "口粮"
 }
